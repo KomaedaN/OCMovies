9dc2834adebf0a38700f3aee04c0b31b7ce5e261
Thomas <84379061+KomaedaN@users.noreply.github.com>
2022-09-01 17:45:34 +0200

    get data from film id

--- js/main.js
+++ js/main.js
@@ -22,6 +22,7 @@ function getMostRatedFilmData() {
         .catch((err) => console.log('Error'))
 }
 
+
 function getFilmsFromCategoryImg(category_url, category, first_page, second_page, id) {
     fetch(Url + category_url)
         .then(res => res.json())
@@ -29,13 +30,38 @@ function getFilmsFromCategoryImg(category_url, category, first_page, second_page
             const next_page = data["next"]
             // get films data from first page 
             for (let i = first_page; i < 5; i++) {
-                let class_name = "category_template__display_" + i
+                current_film_id = data["results"][i]["id"]
+                film_data = this.getDataFromFilmId(current_film_id)
+                console.log(film_data)
+                image_data = data["results"][i]["image_url"]
+                title_data = data["results"][i]["title"]
+                
+                
+                let information_div = document.createElement('div');
+                information_div.id = current_film_id
+
+                document.getElementById("category_" + id).appendChild(information_div)
+
                 let category_id = document.getElementById(category);
                 let template = document.importNode(category_id.content, true);
-                let img = template.getElementById(category + "__img")
+                let img = template.getElementById(category + "__img");
 
-                img.src = data["results"][i]["image_url"]
-                document.getElementById("category_" + id).appendChild(template)
+                img.src = image_data
+                
+                document.getElementById(current_film_id).appendChild(template)
+
+                let title = document.getElementById("category_template__title");
+                let image = document.getElementById("category_template__img_div");
+                let date = document.getElementById("category_template__date");
+
+                title.id = "title_" + current_film_id;
+                image.id = "image_" + current_film_id;
+                date.id = "date_" + current_film_id;
+
+                title.textContent = title_data
+                image.src = image_data
+                
+
             }
             fetch(next_page)
                 .then(res => res.json())
@@ -52,6 +78,20 @@ function getFilmsFromCategoryImg(category_url, category, first_page, second_page
         })
 }
 
+function getDataFromFilmId(id) {
+    return fetch(Url + id)
+        .then(res => res.json())
+        .then(data => {
+            const image_data = data["image_url"];
+            const title_data = data["title"];
+            const date_data = data["date_published"];
+            const rated_data = data["rated"];
+            const imdb_data = data["imdb_score"];
+            return [title_data, date_data]
+        })
+        .catch((err) => console.log(err))
+}
+
 getMostRatedFilmData()
 //category top rated films
 getFilmsFromCategoryImg("?sort_by=-imdb_score", "category_template", 1, 3, "rated")
@@ -61,3 +101,4 @@ getFilmsFromCategoryImg("?sort_by=-imdb_score&genre=history", "category_template
 getFilmsFromCategoryImg("?sort_by=-imdb_score&genre=adventure", "category_template", 0, 2, "adventure")
 //category anime films
 getFilmsFromCategoryImg("?sort_by=-imdb_score&genre=animation", "category_template", 0, 2, "anime")
+
